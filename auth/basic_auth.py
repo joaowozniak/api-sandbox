78d1a279441ba7dfd6b376a2792aa2fa486b0d43
joaowozniak <joao.wozniak@gmail.com>
2021-11-24 19:06:32 +0000

    correct static type check

--- auth/basic_auth.py
+++ auth/basic_auth.py
@@ -3,14 +3,14 @@ from fastapi import HTTPException, Depends
 from fastapi.security.base import SecurityBase
 from fastapi.security.utils import get_authorization_scheme_param
 from starlette.requests import Request
-from typing import Optional
+from typing import Optional, Dict
 
 
 class BasicAuth(SecurityBase):
     def __init__(self, scheme_name: str = None):
         self.scheme_name = scheme_name
 
-    def __call__(self, request: Request) -> (str, bool):
+    def __call__(self, request: Request) -> Dict[str, bool]:
         authorized: str = request.headers.get("Authorization")
         scheme, param = get_authorization_scheme_param(authorized)
 
@@ -24,21 +24,27 @@ class BasicAuth(SecurityBase):
         has_access, has_more_account = verify_user(param)
 
         token = "test_" + has_access
-        return (token, has_more_account)
+        return {"token": token, "has_more_account": has_more_account}
 
 
-def check_username(username: str) -> {str, bool}:
-    if username.startswith("user_multiple_"):
+def check_username(username: str) -> Dict[str, bool]:
+    if (
+        username.startswith("user_multiple_")
+        and username[-5:].isdigit()
+        and len(username) == 19
+    ):
         return {"name": username, "has_more_account": True}
 
-    elif username.startswith("user_"):
+    elif (
+        username.startswith("user_") and username[-5:].isdigit() and len(username) == 10
+    ):
         return {"name": username, "has_more_account": False}
 
     else:
         return {"name": "_" + username, "has_more_account": False}
 
 
-def verify_user(param: str) -> str:
+def verify_user(param: str) -> (str, bool):
 
     decoded = base64.b64decode(param).decode("ascii")
     username, _, password = decoded.partition(":")
--- main.py
+++ main.py
@@ -1,61 +1,64 @@
-import secrets, base64
-
-from fastapi import APIRouter, Depends, HTTPException, FastAPI
+from fastapi import Depends, HTTPException, FastAPI
 from fastapi.security import HTTPBasic
 from fastapi.security.base import SecurityBase
 from fastapi.openapi.models import Response
 
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
-from typing import Optional, List
+from typing import Optional, List, Dict
 from auth.basic_auth import BasicAuth
 
-from controllers.accounts import *
-from controllers.transactions import *
+from controllers.accounts import generate_accounts, get_account_by_id
+from controllers.transactions import generate_transactions, get_transaction_by_id
 
 
 sandbox = FastAPI()
 
 basic_auth = BasicAuth()
 
+print("Starting app...")
 
-def get_current_user(token: str = Depends(basic_auth)) -> (str, bool):
-    return token
+
+def get_current_user(
+    token_dict: Dict[str, bool] = Depends(basic_auth)
+) -> Dict[str, bool]:
+    return token_dict
 
 
 @sandbox.get("/")
 def welcome():
-    return "Hello, navigate to /login to log in."
-
-
-@sandbox.get("/login")
-def login(current_user: (str, bool) = Depends(get_current_user)):
-    return "Welcome, you're logged in."
+    return "Hello. Log in with user_XXXXX where X is digit. user_multiple_XXXXX for user with multiple accounts. Password empty."
 
 
 @sandbox.get("/accounts")
-def get_accounts(current_user: (str, bool) = Depends(get_current_user)):
-    accounts = generate_accounts(current_user[0], current_user[1])
+def get_accounts(current_user: Dict[str, bool] = Depends(get_current_user)):
+    accounts = generate_accounts(
+        current_user["token"], current_user["has_more_account"]
+    )
     accounts_formatted = [acc.show_accounts() for acc in accounts]
     return accounts_formatted
 
 
 @sandbox.get("/accounts/{idx}")
 def get_accounts_by_id(
-    current_user: (str, bool) = Depends(get_current_user), idx: str = None
+    current_user: Dict[str, bool] = Depends(get_current_user), idx: str = None
 ):
-    accounts = generate_accounts(current_user[0], current_user[1])
+    accounts = generate_accounts(
+        current_user["token"], current_user["has_more_account"]
+    )
     account_by_id = get_account_by_id(accounts, idx)
     account_formatted = account_by_id.show_accounts()
     return account_formatted
 
 
 @sandbox.get("/accounts/{idx}/details")
-def get_accounts_by_id(
-    current_user: (str, bool) = Depends(get_current_user), idx: str = None
+def get_accounts_details(
+    current_user: Dict[str, bool] = Depends(get_current_user), idx: str = None
 ):
 
-    accounts = generate_accounts(current_user[0], current_user[1])
+    accounts = generate_accounts(
+        current_user["token"], current_user["has_more_account"]
+    )
     account_by_id = get_account_by_id(accounts, idx)
     if account_by_id is None:
         return "Can't"
@@ -64,10 +67,12 @@ def get_accounts_by_id(
 
 
 @sandbox.get("/accounts/{idx}/balances")
-def get_accounts_by_id(
-    current_user: (str, int) = Depends(get_current_user), idx: str = None
+def get_accounts_balances(
+    current_user: Dict[str, int] = Depends(get_current_user), idx: str = None
 ):
-    accounts = generate_accounts(current_user[0], current_user[1])
+    accounts = generate_accounts(
+        current_user["token"], current_user["has_more_account"]
+    )
     account_by_id = get_account_by_id(accounts, idx)
     if account_by_id is None:
         return "Can't"
@@ -76,10 +81,12 @@ def get_accounts_by_id(
 
 
 @sandbox.get("/accounts/{idx}/transactions")
-def get_accounts_by_id(
-    current_user: (str, int) = Depends(get_current_user), idx: str = None
+def get_transactions(
+    current_user: Dict[str, int] = Depends(get_current_user), idx: str = None
 ):
-    accounts = generate_accounts(current_user[0], current_user[1])
+    accounts = generate_accounts(
+        current_user["token"], current_user["has_more_account"]
+    )
     account_by_id = get_account_by_id(accounts, idx)
     if account_by_id is None:
         return "Can't"
@@ -88,12 +95,14 @@ def get_accounts_by_id(
 
 
 @sandbox.get("/accounts/{idx}/transactions/{idt}")
-def get_accounts_by_id(
-    current_user: (str, int) = Depends(get_current_user),
+def get_transactions_by_id(
+    current_user: Dict[str, int] = Depends(get_current_user),
     idx: str = None,
     idt: str = None,
 ):
-    accounts = generate_accounts(current_user[0], current_user[1])
+    accounts = generate_accounts(
+        current_user["token"], current_user["has_more_account"]
+    )
     account_by_id = get_account_by_id(accounts, idx)
     if account_by_id is None:
         return "Can't"
